1d67780997db85e9116d6f45ebf3bb10bbcdfbae
Mr-Unforgettable <rulzabhinav69@gmail.com>
2024-06-11 17:51:44 +0530

    New database connection logic for sequelize.

--- src/utils/database.ts
+++ src/utils/database.ts
@@ -1,13 +1,44 @@
-import mysql from 'mysql2/promise';
+// import mysql from 'mysql2/promise';
+// import dotenv from 'dotenv';
+//
+// dotenv.config();
+//
+// const pool = mysql.createPool({
+//     host: process.env.DB_HOST,
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PASS,
+//     database: process.env.DB_NAME
+// });
+//
+// export default pool;
+
+import { Sequelize } from "sequelize";
 import dotenv from 'dotenv';
 
 dotenv.config();
 
-const pool = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME
-});
+// Check if environment variables are defined
+if (!process.env.DB_NAME || !process.env.DB_USER || !process.env.DB_PASS || !process.env.DB_HOST) {
+  throw new Error('Environment variables are not properly set.');
+}
 
-export default pool;
+export const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASS,
+  {
+    host: process.env.DB_HOST,
+    dialect: "mysql",
+    logging: false,
+  }
+);
+
+// Test the connection
+(async () => {
+    try {
+        await sequelize.authenticate();
+        console.log('Connection has been established successfully.');
+    } catch (error) {
+        console.error('Unable to connect to the database: ', error);
+    }
+})();
