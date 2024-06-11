88a88ac59cf93813d92d982674b9ce94720fbddc
Mr-Unforgettable <rulzabhinav69@gmail.com>
2024-06-11 17:48:39 +0530

    Added sequelize sync for the models.

--- src/app.ts
+++ src/app.ts
@@ -5,18 +5,10 @@ import adminRoutes from "./routes/admin";
 import shopRoutes from "./routes/shop";
 import pageNotFound from "./routes/404";
 
-import pool from './utils/database';
+import { sequelize } from "./utils/database";
 
 const app = express();
 const PORT = 3000;
-
-pool.execute(`SELECT * FROM shop.products`)
-  .then((results) => {
-    console.log(results[0], results[1]);
-  })
-  .catch(error => {
-    console.error('Error fetching table from the database', error);
-  })
 
 // EJS
 app.set("view engine", "ejs");
@@ -29,6 +21,14 @@ app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 app.use(pageNotFound);
 
-app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}/`);
-});
+sequelize
+  .sync()
+  .then((result) => {
+    console.log(result);
+    app.listen(PORT, () => {
+      console.log(`Server running at http://localhost:${PORT}/`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
