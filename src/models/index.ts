53a85e923c42f34bde588cc0e8c91b3b818d4979
Mr-Unforgettable <rulzabhinav69@gmail.com>
2024-07-04 10:08:06 +0530

    Added new associations between Order, User and Product models.

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,6 +1,8 @@
 import { sequelize } from "../utils/database";
 import { Cart } from "./cart";
 import { CartItem } from "./cart-item";
+import { Order } from "./order";
+import { OrderItem } from "./order-item";
 import { Product } from "./product";
 import { User } from "./user";
 
@@ -17,6 +19,11 @@ Product.belongsToMany(Cart, {
 
 User.hasOne(Cart);
 Cart.belongsTo(User);
+
+Order.belongsTo(User);
+User.hasMany(Order);
+
+Order.belongsToMany(Product, { through: OrderItem });
 
 // Export models
 export { sequelize, Cart, CartItem, Product, User };
