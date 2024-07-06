10c062ece8fe8c3a03434f7fd63cc38f5de9b72a
Mr-Unforgettable <rulzabhinav69@gmail.com>
2024-07-06 12:23:59 +0530

    Implemented the getIndex and getProducts middleware's

--- src/controllers/shop.ts
+++ src/controllers/shop.ts
@@ -1,6 +1,6 @@
 import { RequestHandler } from "express";
 import { Product } from "../models/product";
-import { Cart } from "../models/cart";
+// import { Cart } from "../models/cart";
 
 const handleError = (res: any, error: any, message: string) => {
   console.error(message, error);
@@ -11,41 +11,6 @@ const renderPage = (res: any, view: string, options: any) => {
   res.render(view, options);
 };
 
-export const getProducts: RequestHandler = async (_req, res, _next) => {
-  try {
-    // Fetching the Products list using fetchAll method.
-    const products = await Product.fetchAll();
-    renderPage(res, "shop/product-list", {
-      prods: products,
-      pageTitle: "🛍️ All Products",
-      path: "/products",
-      hasProducts: products.length > 0,
-      editing: false,
-    });
-  } catch (error) {
-    handleError(res, error, "Error fetching products:");
-  }
-};
-
-export const getProduct: RequestHandler = async (req, res, _next) => {
-  try {
-    const prodID = req.params.productID;
-    const product = await Product.findByPk(prodID);
-
-    if (product) {
-      renderPage(res, "shop/product-details", {
-        product: product,
-        pageTitle: `ℹ️ ${product.title} `,
-        path: "product-details",
-      });
-    } else {
-      res.status(404).json({ message: "Product not found" });
-    }
-  } catch (error) {
-    handleError(res, error, "Error fetching products:");
-  }
-};
-
 export const getIndex: RequestHandler = async (_req, res, _next) => {
   try {
     // Fetching the Products list using fetchAll method.
@@ -54,78 +19,113 @@ export const getIndex: RequestHandler = async (_req, res, _next) => {
       prods: products,
       pageTitle: "🛍️ Shop",
       path: "/",
-      hasProducts: products.length > 0,
+      // hasProducts: products.length > 0,
     });
   } catch (error) {
     handleError(res, error, "Error fetching the Product Index:");
   }
 };
 
-export const getCart: RequestHandler = async (_req, res, _next) => {
+export const getProducts: RequestHandler = async (_req, res, _next) => {
   try {
-    const cartProducts = await Cart.fetchAll();
-    const allProducts = await Product.fetchAll();
-    const cartDetails: { productData: Product; qty: number }[] = [];
-
-    for (const cartProduct of cartProducts) {
-      const productData = allProducts.find(prod => prod.id === cartProduct.id);
-      if (productData) {
-        cartDetails.push({ productData, qty: cartProduct.qty });
-      }
-    }
-
-    renderPage(res, "shop/cart", {
-      pageTitle: "🛒 Cart",
-      path: "/cart",
-      prods: cartDetails
+    // Fetching the Products list using fetchAll method.
+    const products = await Product.fetchAll();
+    renderPage(res, "shop/product-list", {
+      prods: products,
+      pageTitle: "🛍️ All Products",
+      path: "/products",
+      editing: false,
+      // hasProducts: products.length > 0,
     });
   } catch (error) {
-    handleError(res, error, "Error fetching the Cart items");
+    handleError(res, error, "Error fetching products:");
   }
 };
 
-export const getCheckout: RequestHandler = (_req, res, _next) => {
-  renderPage(res, "shop/checkout", {
-    pageTitle: "ℹ️ Checkout",
-    path: "/checkout",
-  });
-};
+// export const getProduct: RequestHandler = async (req, res, _next) => {
+//   try {
+//     const prodID = req.params.productID;
+//     const product = await Product.findByPk(prodID);
+// 
+//     if (product) {
+//       renderPage(res, "shop/product-details", {
+//         product: product,
+//         pageTitle: `ℹ️ ${product.title} `,
+//         path: "product-details",
+//       });
+//     } else {
+//       res.status(404).json({ message: "Product not found" });
+//     }
+//   } catch (error) {
+//     handleError(res, error, "Error fetching products:");
+//   }
+// };
 
-export const getOrders: RequestHandler = (_req, res, _next) => {
-  renderPage(res, "shop/orders", {
-    pageTitle: "ℹ️ orders",
-    path: "/orders",
-  });
-};
-
-export const postCart: RequestHandler = async (req, res, _next) => {
-  const prodId = req.body.productId;
-  try {
-    const product = await Product.findByPk(prodId);
-    if (product && product.price !== undefined) {
-      Cart.addProduct(prodId, product.price);
-      res.redirect("/cart");
-    } else {
-      res.status(404).json({ message: "Product not found" });
-    }
-  } catch (error) {
-    handleError(res, error, "Error adding product to cart:");
-  }
-};
-
-export const deleteFromCart: RequestHandler = async (req, res, _next) => {
-  const prodId = req.body.productId;
-  try {
-    const product = await Product.findByPk(prodId);
-    const cartProduct = await Cart.findById(prodId);
-
-    if (product && cartProduct) {
-      await Cart.removeProduct(prodId, product.price);
-      res.redirect("/cart");
-    } else {
-      res.status(404).send({ message: "Product not found" });
-    }
-  } catch (error) {
-    handleError(res, error, "Error deleting product from cart.");
-  }
-};
+// export const getCart: RequestHandler = async (_req, res, _next) => {
+//   try {
+//     const cartProducts = await Cart.fetchAll();
+//     const allProducts = await Product.fetchAll();
+//     const cartDetails: { productData: Product; qty: number }[] = [];
+// 
+//     for (const cartProduct of cartProducts) {
+//       const productData = allProducts.find(prod => prod.id === cartProduct.id);
+//       if (productData) {
+//         cartDetails.push({ productData, qty: cartProduct.qty });
+//       }
+//     }
+// 
+//     renderPage(res, "shop/cart", {
+//       pageTitle: "🛒 Cart",
+//       path: "/cart",
+//       prods: cartDetails
+//     });
+//   } catch (error) {
+//     handleError(res, error, "Error fetching the Cart items");
+//   }
+// };
+// 
+// export const postCart: RequestHandler = async (req, res, _next) => {
+//   const prodId = req.body.productId;
+//   try {
+//     const product = await Product.findByPk(prodId);
+//     if (product && product.price !== undefined) {
+//       Cart.addProduct(prodId, product.price);
+//       res.redirect("/cart");
+//     } else {
+//       res.status(404).json({ message: "Product not found" });
+//     }
+//   } catch (error) {
+//     handleError(res, error, "Error adding product to cart:");
+//   }
+// };
+// 
+// export const getCheckout: RequestHandler = (_req, res, _next) => {
+//   renderPage(res, "shop/checkout", {
+//     pageTitle: "ℹ️ Checkout",
+//     path: "/checkout",
+//   });
+// };
+// 
+// export const getOrders: RequestHandler = (_req, res, _next) => {
+//   renderPage(res, "shop/orders", {
+//     pageTitle: "ℹ️ orders",
+//     path: "/orders",
+//   });
+// };
+// 
+// export const deleteFromCart: RequestHandler = async (req, res, _next) => {
+//   const prodId = req.body.productId;
+//   try {
+//     const product = await Product.findByPk(prodId);
+//     const cartProduct = await Cart.findById(prodId);
+// 
+//     if (product && cartProduct) {
+//       await Cart.removeProduct(prodId, product.price);
+//       res.redirect("/cart");
+//     } else {
+//       res.status(404).send({ message: "Product not found" });
+//     }
+//   } catch (error) {
+//     handleError(res, error, "Error deleting product from cart.");
+//   }
+// };
