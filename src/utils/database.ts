02308d75b4b8837a670809dacbeaeeb4d279c3d7
Mr-Unforgettable <rulzabhinav69@gmail.com>
2024-07-06 11:40:17 +0530

    Proper database lifecycle handling and connection reuse.

--- src/utils/database.ts
+++ src/utils/database.ts
@@ -5,8 +5,11 @@ dotenv.config();
 
 const uri = process.env.URI!;
 let _db: any;
+let _client: MongoClient | null = null;
 
-export const database = (callback?: any) => {
+export const connectDB = async(callback?: any) => {
+  if (_db) return _db;
+
   // Create a MongoClient with a MongoClientOptions object to set the Stable API version
   const client = new MongoClient(uri, {
     serverApi: {
@@ -16,32 +19,41 @@ export const database = (callback?: any) => {
     },
   });
 
-  async function run() {
-    try {
-      // Connect the client to the server	(optional starting in v4.7)
-      await client.connect();
-      // Send a ping to confirm a successful connection
-      await client.db("shop").command({ ping: 1 });
-      console.log(
-        "Pinged your deployment. You successfully connected to MongoDB!"
-      );
-      _db = client.db();
-      callback();
-      // Get the server address and any other relevant client information
-    } catch (error: any) {
-      console.log(error);
-      throw error;
-    } finally {
-      // Ensures that the client will close when you finish/error
-      await client.close();
-    }
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("shop").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+    callback();
+    _client = client;
+    _db = client.db();
+    return _db;
+    // Get the server address and any other relevant client information
+  } catch (error: any) {
+    console.log(error);
+    throw error;
   }
-  run().catch(console.dir);
 };
 
 export const getDB = () => {
   if (_db) {
     return _db;
   }
-  throw 'No database found!';
-}
+  throw "No database found!";
+};
+
+export const closeDB = async() => {
+  try {
+    if (_client) {
+      await _client.close();
+      console.log("MongoDB connection closed.");
+      _db = null;
+      _client = null;
+    }
+  } catch (error: any) {
+    console.error("Error closing MongoDB connection:", error);
+  }
+};
