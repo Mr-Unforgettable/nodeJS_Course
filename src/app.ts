337ae4bb69ca337ffa5e7705c70cf296f3bbcf24
Mr-Unforgettable <rulzabhinav69@gmail.com>
2024-07-06 11:37:59 +0530

    Refactoring app init and additional checks.

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,9 @@
 import express from "express";
 import path from "node:path";
 
-import { database } from "./utils/database";
+import { closeDB, connectDB } from "./utils/database";
 
-// import adminRoutes from "./routes/admin";
+import adminRoutes from "./routes/admin";
 //import shopRoutes from "./routes/shop";
 //import pageNotFound from "./routes/404";
 
@@ -17,12 +17,32 @@ app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
-// app.use("/admin", adminRoutes);
+app.use("/admin", adminRoutes);
 //app.use(shopRoutes);
 //app.use(pageNotFound);
 
-database(() => {
+connectDB(() => {
   app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}/`);
   });
+});
+
+process.on('SIGINT', async () => {
+  try {
+    await closeDB();
+    process.exit(0);
+  } catch (error) {
+    console.error('Error closing MongoDB connection:', error);
+    process.exit(1);
+  }
+});
+
+process.on('SIGTERM', async () => {
+  try {
+    await closeDB();
+    process.exit(0);
+  } catch (error) {
+    console.error('Error closing MongoDB connection:', error);
+    process.exit(1);
+  }
 });
