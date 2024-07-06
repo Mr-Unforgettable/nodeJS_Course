e2d52681e7192e2dc0c97524147b383444606f6c
Mr-Unforgettable <rulzabhinav69@gmail.com>
2024-07-06 11:39:37 +0530

    Temp disabled other routes.

--- src/routes/admin.ts
+++ src/routes/admin.ts
@@ -1,31 +1,27 @@
 import { Router } from "express";
 import {
   getAddProduct,
-  getEditProduct,
   postAddProduct,
-  getAdminProducts,
-  postEditProduct,
-  postDeleteProduct,
 } from "../controllers/admin";
 
 const router = Router();
 
 // /admin/add-product => GET
 router.get("/add-product", getAddProduct);
-
-// /admin/products => GET
-router.get("/products", getAdminProducts);
-
-// /edit-product/:productId => GET
-router.get("/edit-product/:productID", getEditProduct);
-
-// /edit-product => POST
-router.post("/edit-product", postEditProduct);
-
 // /admin/add-product => POST
 router.post("/add-product", postAddProduct);
 
+// /admin/products => GET
+//router.get("/products", getAdminProducts);
+
+// /edit-product/:productId => GET
+//router.get("/edit-product/:productID", getEditProduct);
+
+// /edit-product => POST
+//router.post("/edit-product", postEditProduct);
+
+
 // /admin/delete-product => DELETE
-router.post("/delete-product", postDeleteProduct);
+//router.post("/delete-product", postDeleteProduct);
 
 export default router;
