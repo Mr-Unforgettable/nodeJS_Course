3a856010a9d8039451e8a975e339ac3a4204ab70
Mr-Unforgettable <rulzabhinav69@gmail.com>
2024-07-08 21:25:19 +0530

    Added addToCart method to add cart for a user.

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,8 +1,14 @@
 import { ObjectId } from "mongodb";
 import { getDB } from "../utils/database";
+import { Product } from "./product";
 
 export class User {
-  constructor(public user: string, public email: string, _id: ObjectId) {}
+  constructor(
+    public name: string,
+    public email: string,
+    public cart?: any,
+    public _id?: ObjectId
+  ) {}
 
   async save() {
     // Get the database connection from the connection pool.
@@ -10,13 +16,52 @@ export class User {
     // Use the InsertOne() operation on the data and run the query.
     try {
       const newUser = await db.collection("users").insertOne({
-        user: this.user,
+        name: this.name,
         email: this.email,
       });
       console.log(`User saved successfully: ${newUser.insertedId}`);
       return newUser.insertedId;
     } catch (error) {
       console.error(`failed to insert a new user: ${error}`);
+      throw error;
+    }
+  }
+
+  async addToCart(product: Product) {
+    const db = getDB();
+
+    try {
+      const cartProductIndex = this.cart.items.findIndex((item: any) => {
+        return item.productId.toString() === product._id?.toString();
+      });
+
+      let newQuantity = 1;
+      const updatedCartItem = [...this.cart.items];
+
+      if (cartProductIndex >= 0) {
+        newQuantity = this.cart.items[cartProductIndex].quantity + 1;
+        updatedCartItem[cartProductIndex].quantity = newQuantity;
+      } else {
+        // Passing the product reference along with a new overrided property
+        updatedCartItem.push({
+          productId:
+            product._id !== null ? new ObjectId(product._id) : undefined,
+          quantity: newQuantity,
+        });
+      }
+
+      const updatedCart = {
+        items: updatedCartItem,
+      };
+      const updateCart = await db.collection("users").updateOne(
+        {
+          _id: new ObjectId(this._id),
+        },
+        { $set: { cart: updatedCart } }
+      );
+      return updateCart;
+    } catch (error) {
+      console.error(`failed to add product to cart: ${error}`);
       throw error;
     }
   }
