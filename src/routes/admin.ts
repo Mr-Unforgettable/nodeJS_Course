701ed01160c94416405e4a9866f6ee439a2e99cd
Mr-Unforgettable <rulzabhinav69@gmail.com>
2024-07-11 20:52:06 +0530

    Refactoring and cleanup.

--- src/routes/admin.ts
+++ src/routes/admin.ts
@@ -12,15 +12,16 @@ const router = Router();
 
 // /admin/add-product => GET
 router.get("/add-product", getAddProduct);
+
 // /admin/add-product => POST
 router.post("/add-product", postAddProduct);
 
 // /admin/products => GET
 router.get("/products", getAdminProducts);
-
+ 
 // /edit-product/:productId => GET
 router.get("/edit-product/:productID", getEditProduct);
-
+ 
 // /edit-product => POST
 router.post("/edit-product", postEditProduct);
 
