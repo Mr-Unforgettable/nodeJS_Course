4023c0042c424428d113e32836723840f8a6ac92
Mr-Unforgettable <rulzabhinav69@gmail.com>
2024-08-15 20:16:13 +0530

    Simple session token generation.

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -1,13 +1,26 @@
 import { RequestHandler } from "express";
 
-export const getLogin: RequestHandler = async (_req, res, _next) => {
+export const getLogin: RequestHandler = async (req, res, _next) => {
   try {
+    const isLoggedIn = req.get("Cookie")?.split("=")[1].trim();
     res.render("auth/login", {
       pageTitle: "🔐 Login",
+      isAuthenticated: isLoggedIn,
       path: "/login",
     });
   } catch (error) {
     console.error("failed to open the login page.");
     res.status(500).send({ message: "Internal Server Error." });
+  }
+};
+
+export const postLogin: RequestHandler = async (req, res, _next) => {
+  try {
+  //  res.setHeader("Set-Cookie", "isLoggedIn=true; HttpOnly");
+    req.session.isLoggedIn = true;
+    res.redirect("/");
+  } catch (error) {
+    console.error("Invalid credentials.");
+    res.status(403).send({ message: "Not Authorized." });
   }
 };
