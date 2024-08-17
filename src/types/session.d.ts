d796b61c27088710e4182bc5568a106c4d31c8ed
Mr-Unforgettable <rulzabhinav69@gmail.com>
2024-08-17 15:30:03 +0530

    Added a new property in our custom session type definition file.

--- src/types/session.d.ts
+++ src/types/session.d.ts
@@ -1,7 +1,9 @@
 import 'express-session';
+import { User } from '../models/user';
 
 declare module 'express-session' {
     interface SessionData {
+        user?: User,
         isLoggedIn?: boolean,
     }
 }
