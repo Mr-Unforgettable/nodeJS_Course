2ddfe6c1ad1c3a307b1fc98b351d42d51109706b
Mr-Unforgettable <rulzabhinav69@gmail.com>
2024-08-17 15:53:45 +0530

    Fixed the issue with some UI elements not getting rendered on the page.

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -1,11 +1,12 @@
 import { RequestHandler } from "express";
+import { User } from "../models/user";
 
 export const getLogin: RequestHandler = async (req, res, _next) => {
   try {
-    const isLoggedIn = req.get("Cookie")?.split("=")[1].trim();
+    // const isLoggedIn = req.get("Cookie")?.split("=")[1].trim();
     res.render("auth/login", {
       pageTitle: "🔐 Login",
-      isAuthenticated: isLoggedIn,
+      isAuthenticated: false,
       path: "/login",
     });
   } catch (error) {
@@ -16,11 +17,31 @@ export const getLogin: RequestHandler = async (req, res, _next) => {
 
 export const postLogin: RequestHandler = async (req, res, _next) => {
   try {
-  //  res.setHeader("Set-Cookie", "isLoggedIn=true; HttpOnly");
-    req.session.isLoggedIn = true;
-    res.redirect("/");
+    //  res.setHeader("Set-Cookie", "isLoggedIn=true; HttpOnly");
+    const user = await User.findById("6690e25c129cb3961870bdc4");
+    if (user) {
+      req.session.isLoggedIn = true;
+      req.session.user = user;
+      req.session.save((err) => {
+        if (err) {
+          console.log(err);
+        }
+        res.redirect("/");
+      });
+    }
   } catch (error) {
     console.error("Invalid credentials.");
     res.status(403).send({ message: "Not Authorized." });
+  }
+};
+
+export const postLogout: RequestHandler = async (req, res, _next) => {
+  try {
+    req.session.destroy(() => {
+      res.redirect("/");
+    });
+  } catch (error) {
+    console.error("Something went wrong.");
+    res.status(500).send({ message: "Internal Server Error." });
   }
 };
