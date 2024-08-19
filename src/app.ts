f216151f771d44694e3c7b6f81b8af0b4306fb7a
Mr-Unforgettable <rulzabhinav69@gmail.com>
2024-08-19 18:14:00 +0530

    Removed unused imports

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express, { NextFunction, Request, Response } from "express";
+import express from "express";
 import path from "node:path";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
