685cf444f4dc343538c12a961a1c3b7055e1276e
Mr-Unforgettable <rulzabhinav69@gmail.com>
2024-10-03 20:28:48 +0530

    Added middleware functions for handling user creation and authentication

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -1,9 +1,9 @@
 import { RequestHandler } from "express";
 import { User } from "../models/user";
+import bcrypt from "bcryptjs";
 
-export const getLogin: RequestHandler = async (req, res, _next) => {
+export const getLogin: RequestHandler = async (_req, res, _next) => {
   try {
-    // const isLoggedIn = req.get("Cookie")?.split("=")[1].trim();
     res.render("auth/login", {
       pageTitle: "🔐 Login",
       isAuthenticated: false,
@@ -17,21 +17,81 @@ export const getLogin: RequestHandler = async (req, res, _next) => {
 
 export const postLogin: RequestHandler = async (req, res, _next) => {
   try {
-    //  res.setHeader("Set-Cookie", "isLoggedIn=true; HttpOnly");
-    const user = await User.findById("6690e25c129cb3961870bdc4");
-    if (user) {
-      req.session.isLoggedIn = true;
-      req.session.user = user;
-      req.session.save((err) => {
-        if (err) {
-          console.log(err);
-        }
-        res.redirect("/");
-      });
+    const { email, password } = req.body;
+    const user = await User.findOne({ email: email });
+
+    if (!user) {
+      res.redirect("/login");
+      return;
     }
+
+    const isCorrectPasswd = await bcrypt.compare(password, user.password);
+
+    if (!isCorrectPasswd) {
+      res.redirect("/login");
+      return;
+    }
+
+    req.session.isLoggedIn = true;
+    req.session.user = user;
+    req.session.save((err: Error) => {
+      console.log(err);
+      res.redirect("/");
+    });
+
   } catch (error) {
-    console.error("Invalid credentials.");
-    res.status(403).send({ message: "Not Authorized." });
+    console.error("failed to login");
+    res.status(401).send({ message: "Not Authorized" });
+  }
+};
+
+
+export const getSignup: RequestHandler = async (_req, res) => {
+  try {
+    res.render("auth/signup", {
+      pageTitle: "📋 Sign Up",
+      isAuthenticated: false,
+      path: "/signup",
+    });
+  } catch (error) {
+    console.error("Failed to fetch the signup page.");
+    res.status(500).send({ message: "Internal Server Error." });
+  }
+};
+
+export const postSignup: RequestHandler = async (req, res) => {
+  try {
+    const { email, password, confirmPassword } = req.body;
+
+    if (!email || !password || !confirmPassword) {
+      res.status(400).send({ message: "Bad Request" });
+      return;
+    };
+
+    // Check if the user already exists in the database
+    const userEmail = await User.findOne({ email: email });
+    if (userEmail) {
+      res.redirect('/signup');
+      return;
+    }
+
+    const username = email.split('@')[0];
+    const hashedPassword = await bcrypt.hash(password, 12);
+
+    const user = new User({
+      name: username,
+      email: email,
+      password: hashedPassword,
+      cart: { items: [] },
+    });
+
+    await user.save();
+    res.redirect('/login');
+  } catch (error) {
+    console.error("failed to create a new user");
+    res.status(500).send({
+      message: "Internal Server Error."
+    });
   }
 };
 
