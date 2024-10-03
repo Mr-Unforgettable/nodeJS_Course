ea5709cd425bba215ed66b1ceb22b71f9abab807
Mr-Unforgettable <rulzabhinav69@gmail.com>
2024-10-03 20:32:48 +0530

    Added arbitrary routes access control mechanism

--- src/routes/shop.ts
+++ src/routes/shop.ts
@@ -1,39 +1,41 @@
 import { Router } from "express";
 import {
-getIndex,
-getProducts,
-getProduct,
-getCart,
-postCart,
-deleteFromCart,
-getOrders,
-postOrders
+  getIndex,
+  getProducts,
+  getProduct,
+  getCart,
+  postCart,
+  deleteFromCart,
+  getOrders,
+  postOrders
 } from "../controllers/shop";
+
+import { isAuth } from "../middleware/is_auth";
 
 const router = Router();
 
 // Home route => GET
-router.get("/", getIndex);
+router.get("/", isAuth, getIndex);
 
 // /products => GET
 router.get("/products", getProducts);
 
 // /products/[:id] => GET
-router.get("/products/:productID", getProduct);
+router.get("/products/:productID", isAuth, getProduct);
 
 // Cart route => GET
-router.get("/cart", getCart);
+router.get("/cart", isAuth, getCart);
 
 // /cart => POST
-router.post("/cart", postCart);
+router.post("/cart", isAuth, postCart);
 
 // /cart => POST
-router.post("/cart-delete-item", deleteFromCart);
+router.post("/cart-delete-item", isAuth, deleteFromCart);
 
 // /orders => GET
-router.get("/orders", getOrders);
+router.get("/orders", isAuth, getOrders);
 
 // /orders => POST
-router.post("/create-order", postOrders);
+router.post("/create-order", isAuth, postOrders);
 
 export default router;
