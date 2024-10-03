4d7d2b0be1ec77c7fa14f62fd96083d0abc023aa
Mr-Unforgettable <rulzabhinav69@gmail.com>
2024-10-03 20:30:51 +0530

    Added a arbitrary route access control mechanism

--- src/routes/admin.ts
+++ src/routes/admin.ts
@@ -7,25 +7,26 @@ import {
   postEditProduct,
   postDeleteProduct,
 } from "../controllers/admin";
+import { isAuth } from "../middleware/is_auth";
 
 const router = Router();
 
 // /admin/add-product => GET
-router.get("/add-product", getAddProduct);
+router.get("/add-product", isAuth, getAddProduct);
 
 // /admin/add-product => POST
-router.post("/add-product", postAddProduct);
+router.post("/add-product", isAuth, postAddProduct);
 
 // /admin/products => GET
-router.get("/products", getAdminProducts);
- 
+router.get("/products", isAuth, getAdminProducts);
+
 // /edit-product/:productId => GET
-router.get("/edit-product/:productID", getEditProduct);
- 
+router.get("/edit-product/:productID", isAuth, getEditProduct);
+
 // /edit-product => POST
-router.post("/edit-product", postEditProduct);
+router.post("/edit-product", isAuth, postEditProduct);
 
 // /admin/delete-product => DELETE
-router.post("/delete-product", postDeleteProduct);
+router.post("/delete-product", isAuth, postDeleteProduct);
 
 export default router;
