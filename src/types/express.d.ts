da94f1cc597917009070b3cbd0cf58ba109d76ad
Mr-Unforgettable <rulzabhinav69@gmail.com>
2024-10-16 21:44:55 +0530

    Added a new custom type

--- src/types/express.d.ts
+++ src/types/express.d.ts
@@ -1,10 +1,14 @@
-import 'express';
-import { User } from '../models/user';
+import "express";
+import { User } from "../models/user";
 
 declare global {
-    namespace Express {
-        interface Request {
-            user?: User;
-        }
+  namespace Express {
+    interface Request {
+      user?: User;
     }
+
+    type csrfToken = {
+      csrfToken: () => string;
+    };
+  }
 }
